c325e9c18c704c2ac590704973737b39b4e1d1f7
ismailcet <ismailcet3@gmail.com>
2022-02-24 22:21:48 +0300

    Day-11 Project

--- Custom HTML5 Video Player/script.js
+++ Custom HTML5 Video Player/script.js
@@ -5,3 +5,57 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+//Functions
+function togglePlay(){
+    if(video.paused){
+        video.play();
+    }
+    else{
+        video.pause();
+    }
+}
+function changeIcon(){
+    if(video.paused){
+        toggle.textContent='►';
+    }else{
+        toggle.textContent='❚ ❚';
+    }
+}
+function skip(e){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate(){
+    video[this.name]=this.value;
+}
+function handleProgress(){
+    const percent=(video.currentTime / video.duration)*100;
+    progressBar.style.flexBasis=`${percent}%`;
+}
+function scrub(e){
+    const changeTime=(e.offsetX / progress.offsetWidth)*video.duration;
+    video.currentTime=changeTime;
+}
+
+//Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', changeIcon);
+video.addEventListener('pause', changeIcon);
+video.addEventListener('timeupdate',handleProgress);
+
+toggle.addEventListener('click',togglePlay);
+toggle.addEventListener('click',changeIcon);
+
+skipButtons.forEach(skipButton=>skipButton.addEventListener('click',skip));
+
+ranges.forEach(range=>{
+    range.addEventListener('change',handleRangeUpdate)
+    range.addEventListener('mousemove',handleRangeUpdate)    
+});
+
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
